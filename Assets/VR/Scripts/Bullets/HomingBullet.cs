bc9c7e2c56f358d132ef8bc8fedfe86d0fa4ee66
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-05-10 16:11:55 +1000

    Homing bullets or something

--- Assets/VR/Scripts/Bullets/HomingBullet.cs
+++ Assets/VR/Scripts/Bullets/HomingBullet.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class HomingBullet : Bullet
 {
-    Transform playerTransform;
+    //Transform playerTransform;
+    private Vector3 playerPosition;
+    private float maxSpeed;
 
     private void Awake()
     {
         bulletRigidbody = GetComponent<Rigidbody>();
         bulletRigidbody.isKinematic = true;
         particle = GetComponent<ParticleSystem>();
-        playerTransform = GameObject.FindWithTag("Player").transform;
+        playerPosition = GameObject.FindWithTag("Player").transform.position;
+        maxSpeed = (transform.position - playerPosition).magnitude;
     }
 
     protected override void Move()
     {
-        transform.Translate(velocity * Time.deltaTime, Space.Self);
+        float speed = (transform.position - playerPosition).magnitude;
+        transform.Translate(velocity * Time.deltaTime * (speed/maxSpeed), Space.Self);
     }
 
     protected override void Spin()
     {
-        Vector3 targetVector = transform.InverseTransformPoint(playerTransform.position).normalized;
+        /*
+        Vector3 targetVector = transform.InverseTransformPoint(playerTransform.position);
         Quaternion targetRotation = Quaternion.LookRotation(targetVector);
         
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, angularVelocity.x * Time.deltaTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, angularVelocity.x);
+        */
+        transform.LookAt(playerPosition);
     }
 }
