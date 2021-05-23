b2f1f1b5e3b5e11ba527093394165f1afd06fccf
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-05-24 00:37:28 +1000

    Created script for bullet object pooling

---
+++ Assets/VR/Scripts/Bullets/BulletPool.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPool : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> bulletWaves;//This should have seven objects in it
+    //On the first phase it will spawn the first three bullet prefabs
+    //When the phase changes, the set of bullets will move up the list by one
+    //Each pattern is harder than the previous ones
+    private int waveMarker = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnBullet()
+    {
+
+    }
+
+    public static void NextPhase()
+    {
+
+    }
+}
